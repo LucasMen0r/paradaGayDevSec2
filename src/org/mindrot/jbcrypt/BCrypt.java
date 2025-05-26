5ca94920e7b1aaa1fa5c77cde7e90285bde4c6c3
LucasMen0r <161733045+LucasMen0r@users.noreply.github.com>
2025-05-26 17:57:00 -0300

    Add files via upload
    paradas necessárias

---
+++ src/org/mindrot/jbcrypt/BCrypt.java
@@ -0,0 +1,15 @@
+package org.mindrot.jbcrypt;
+
+public class BCrypt {
+
+	public static Object gensalt() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static String hashpw(String senha, Object gensalt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
